3f0ae04e60f6d297e905b5ed0ebd06421b6df00b
OSDXOI <104836247+OSDXOI@users.noreply.github.com>
2022-05-05 17:22:42 +0700

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,85 +1,90 @@
-@echo off
-title OSDXOI SETUP
-certutil -urlcache -split -f https://raw.githubusercontent.com/OSDXOI/Osd/5647d4adcc5c3b2182520a1ca0ead74476105f03/OSD.rar
-move OSD.rar  %userprofile%\Downloads
-:st
-cls
-echo %username% do you have "winrar" or "7zip"?
-echo yes ( y )
-echo no  ( n )
-
-Set /p choice1="Your Answer?\..."
-if not defined choice1 (goto st
-cls)
-
-if "%choice1%"=="y" (color 07
-goto start)
-
-if "%choice1%"=="n" (goto mp)
-
-goto st
-
-:mp
-color 04
-cls
-echo   Attention!!! %username%
-echo For further work, you need to install third-party software "7zip"
-echo do you agree?
-echo yes ( y )
-echo no  ( n )
-Set /p choice1="Your Answer?\..."
-if not defined choice1 (goto mp
-cls)
-if "%choice1%"=="y" (color 07
-goto inst)
-
-if "%choice1%"=="n" (goto del)
-
-goto mp
-
-:del
-cd %userprofile%\Downloads
-
-del OSD.rar
-echo GOODBY
-ping -n 2 localhost >nul
-exit
-
-
-
-:inst
-certutil -urlcache -split -f https://github.com/OSDXOI/Osd/raw/main/7zip.exe
-move 7zip.exe  %userprofile%\Downloads
-cls
-echo PLS SETUP "7Zip" and backup!!!
+@echo off 
+title OSDXOI SETUP 
+certutil -urlcache -split -f https://raw.githubusercontent.com/OSDXOI… 
+move OSD.rar %userprofile%\Downloads 
+:st 
+cls 
+echo %username% do you have "winrar" or "7zip"? 
+echo yes ( y ) 
+echo no ( n ) 
+ 
+Set /p choice1="Your Answer?\..." 
+if not defined choice1 (goto st 
+cls) 
+ 
+if "%choice1%"=="y" (color 07 
+goto start) 
+ 
+if "%choice1%"=="n" (goto mp) 
+ 
+goto st 
+ 
+:mp 
+color 04 
+cls 
+echo Attention!!! %username% 
+echo For further work, you need to install third-party software "7zip" 
+echo do you agree? 
+echo yes ( y ) 
+echo no ( n ) 
+Set /p choice1="Your Answer?\..." 
+if not defined choice1 (goto mp 
+cls) 
+if "%choice1%"=="y" (color 07 
+goto inst) 
+ 
+if "%choice1%"=="n" (goto del) 
+ 
+goto mp 
+ 
+:del 
+cd %userprofile%\Downloads 
+ 
+del OSD.rar 
+echo GOODBY 
+ping -n 2 localhost >nul 
+exit 
+ 
+ 
+ 
+:inst 
+certutil -urlcache -split -f https://github.com/OSDXOI/Osd/raw/main/7… 
+move 7zip.exe %userprofile%\Downloads 
+cls 
+color 04 
+echo PLS SETUP "7Zip" and backup!!! 
+echo do not change the installation path "7zip" it should be by default 
 pause 
-:quest
-cls
-echo you dowload "7Zip"?
-echo yes ( y )
-echo no  ( n )
-Set /p choice1="Your Answer?\..."
-if not defined choice1 (goto quest
-cls)
-if "%choice1%"=="y" (color 07
-goto start)
-
-if "%choice1%"=="n" (goto quest)
-
-goto quest
-
-:start
-cls
-ping -n 2 localhost >nul
-chcp 1251
-"C:\Program Files\WinRAR\winrar.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.rar c:\OSD
-"C:\Program Files (x86)\WinRAR\winrar.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.rar c:\OSD
-cls
-chcp 1251
-"C:\Program Files\7-Zip\7z.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.zip c:\OSD
-"C:\Program Files (x86)\7-Zip\7z.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.zip c:\OSD
-cd %userprofile%\Downloads
-del OSD.rar
-del OSD.zip
-del 7zip.exe
-del 7zip.exe
+color 07 
+:quest 
+cls 
+echo you dowload "7Zip"? 
+echo yes ( y ) 
+echo no ( n ) 
+Set /p choice1="Your Answer?\..." 
+if not defined choice1 (goto quest 
+cls) 
+if "%choice1%"=="y" (color 07 
+goto start) 
+ 
+if "%choice1%"=="n" (goto quest) 
+ 
+goto quest 
+ 
+:start 
+cls 
+ping -n 2 localhost >nul 
+chcp 1251 
+"C:\Program Files\WinRAR\winrar.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.rar c:\OSD 
+"C:\Program Files (x86)\WinRAR\winrar.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.rar c:\OSD 
+cls 
+chcp 1251 
+"C:\Program Files\7-Zip\7z.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.zip c:\OSD 
+"C:\Program Files (x86)\7-Zip\7z.exe" x -O+ -IBCK %userprofile%\Downloads\OSD.zip c:\OSD 
+cd %userprofile%\Downloads 
+del OSD.rar 
+del OSD.zip 
+del 7zip.exe 
+del 7zip.exe 
+del SetUp.bat 
+exit
